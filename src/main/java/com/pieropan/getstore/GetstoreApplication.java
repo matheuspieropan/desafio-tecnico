7c982a1c01c146fc8f14087a1c85178b0fd83c08
mamapisouza <Pi204326@>
2023-07-27 14:19:27 -0300

    first commit

---
+++ src/main/java/com/pieropan/getstore/GetstoreApplication.java
@@ -0,0 +1,13 @@
+package com.pieropan.getstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pieropan/getstore/GetstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pieropan.getstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

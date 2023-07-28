e93e17ebeea1c04e90d1b2500dc4c4608626c397
mamapisouza <Pi204326@>
2023-07-27 22:12:14 -0300

    Início da documentação

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# get-store
+# Autenticação com Spring security 😄 
+
+Como solicitado no exercício o sistema possui autenticação. Utilizei o security do spring para realização do mesmo.
+
+<h3>Funcionamento:</h3> O endpoint /usuario/login espera no corpo da requisição um e-mail e senha. Ao realizar a validação, o sistema retorna um token JWT com expiração de 3 horas. Esse mesmo token deverá ser informado no cabeçalho Authorization em cada requisição.
+
+<i>OBS: Para testar o login, é necessário se cadastrar através do endpoint /usuario </i>
+
+---
+<h3>Para facilitar:</h3> Ao inicializar a aplicação eu cadastrei manualmente um usuário para teste. Sendo assim, você não precisa necessariámente se cadastrar para testar as requisições. Você pode utilizar as credenciais abaixo: <br><br>
+e-mail: admin@admin.com <br>
+senha: 123
+
+---
--- src/main/java/com/pieropan/getstore/GetStoreApplication.java
+++ src/main/java/com/pieropan/getstore/GetStoreApplication.java
@@ -1,12 +1,30 @@
 package com.pieropan.getstore;
 
+import com.pieropan.getstore.entity.Usuario;
+import com.pieropan.getstore.repository.UsuarioRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@AllArgsConstructor
 @SpringBootApplication
 public class GetStoreApplication {
 
+    UsuarioRepository repository;
+
+    PasswordEncoder passwordEncoder;
+
     public static void main(String[] args) {
         SpringApplication.run(GetStoreApplication.class, args);
+    }
+
+    @Bean
+    public void instanciarUsuario() {
+        boolean semUsuarios = !repository.findAll().iterator().hasNext();
+        if (semUsuarios) {
+            repository.save(new Usuario("admin", "admin@admin.com", passwordEncoder.encode("123")));
+        }
     }
 }
